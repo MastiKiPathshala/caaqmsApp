541a3b0295c7adef024b58c677fc6b3e0f6ffa09
romanova99 <djmenaal@gmail.com>
2017-05-30 19:17:32 +0530

    Add files via upload

--- public/js/location.js
+++ public/js/location.js
@@ -1,3 +1,19 @@
+/*************************************************************************
+ *
+ * $file: location.js
+ *
+ * @brief: web App back-end code, reading data from blobs and sending
+ * it to browser.
+ *
+ * @author1: Taniya Datta 
+ * @author2: Saurabh Singh
+ *
+ * @date: 15 May 2017 First version of web app back-end code
+ *
+ * This file is subject to the terms and conditions defined in
+ * file 'LICENSE.txt', which is part of this source code package.
+ *
+ ************************************************************************/
 var locationInit = function () 
 {
 	var locationSet = [];
@@ -8,10 +24,10 @@ var locationInit = function ()
 	}).done(function(data) {
 
 		if (data.status === "OK") {
-			for(var idx = 0; idx < data.results.lat.length; idx++){
+			for(var i = 0; i < data.results.lat.length; i++){
 				
 				console.log(data.results);
-			    gatewayLocation = { lat: data.results.lat[idx], lng: data.results.lng[idx] ,quality: data.results.airQuality[idx]};
+			    gatewayLocation = { gatewayId: data.results.gatId[i], lat: data.results.lat[i], lng: data.results.lng[i] ,quality: data.results.airQuality[i]};
 				
 			    console.log ("SecurIoT Gateway location: " + JSON.stringify(gatewayLocation));
 			    locationSet.push (gatewayLocation);
@@ -19,17 +35,102 @@ var locationInit = function ()
 			}
 		} else {
 			
-			gatewayLocation = { lat: 12.963778, lng: 77.712111 };
+			gatewayLocation = { gatewayId: "b8:27:eb:94:7c:9d" ,lat:12.963778 , lng: 77.712111 ,quality: 30};
+			locationSet.push (gatewayLocation);
+			alert(data.results);
 		}
+		var styledMapType = new google.maps.StyledMapType(
+            [
+              {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
+            {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
+            {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
+            {
+              featureType: 'administrative.locality',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#d59563'}]
+            },
+            {
+              featureType: 'poi',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#d59563'}]
+            },
+            {
+              featureType: 'poi.park',
+              elementType: 'geometry',
+              stylers: [{color: '#263c3f'}]
+            },
+            {
+              featureType: 'poi.park',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#6b9a76'}]
+            },
+            {
+              featureType: 'road',
+              elementType: 'geometry',
+              stylers: [{color: '#38414e'}]
+            },
+            {
+              featureType: 'road',
+              elementType: 'geometry.stroke',
+              stylers: [{color: '#212a37'}]
+            },
+            {
+              featureType: 'road',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#9ca5b3'}]
+            },
+            {
+              featureType: 'road.highway',
+              elementType: 'geometry',
+              stylers: [{color: '#746855'}]
+            },
+            {
+              featureType: 'road.highway',
+              elementType: 'geometry.stroke',
+              stylers: [{color: '#1f2835'}]
+            },
+            {
+              featureType: 'road.highway',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#f3d19c'}]
+            },
+            {
+              featureType: 'transit',
+              elementType: 'geometry',
+              stylers: [{color: '#2f3948'}]
+            },
+            {
+              featureType: 'transit.station',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#d59563'}]
+            },
+            {
+              featureType: 'water',
+              elementType: 'geometry',
+              stylers: [{color: '#17263c'}]
+            },
+            {
+              featureType: 'water',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#515c6d'}]
+            },
+            {
+              featureType: 'water',
+              elementType: 'labels.text.stroke',
+              stylers: [{color: '#17263c'}]
+            }
+            ],
+            {name: 'Styled Map'});
 		
-
 		map = new google.maps.Map(document.getElementById('map'), {
 			zoom: 5,
 			mapTypeId: 'satellite',
 			center: gatewayLocation
 		});
-		var infowindow = new google.maps.InfoWindow();
-		 map.addListener('center_changed', function() {
+		map.mapTypes.set('styled_map', styledMapType);
+        map.setMapTypeId('styled_map');
+		//var infowindow = new google.maps.InfoWindow();
+		map.addListener('center_changed', function() {
           // 3 seconds after the center of the map has changed, pan back to the
           // marker.
           window.setTimeout(function() {
@@ -39,17 +140,19 @@ var locationInit = function ()
 
 		/*
 		var heatmap = new google.maps.visualization.HeatmapLayer({
-Ã‚Â  			data: locations
+Â  			data: locations
 		});
 		heatmap.setMap(map);
 		*/
-		image = '/images/green-dot.png';
+		//image = '/images/green-dot.png';
 		locationSet.forEach(function (gatewayLocation) {
-			if( gatewayLocation.quality <= 40) {
+			if( gatewayLocation.quality >=60){
 					
 					image = '/images/green-dot.png';
+
+
 				}
-				else if( gatewayLocation.quality > 40 && gatewayLocation.quality <= 60) {
+				else if( gatewayLocation.quality > 40 && gatewayLocation.quality < 60) {
 					
 					image = '/images/yellow-dot.png';
 				}
@@ -61,50 +164,79 @@ var locationInit = function ()
 				position: gatewayLocation,
 				icon: image,
 				map: map,
-                //title :'b8:22:e7:11:3b'
+				title: gatewayLocation.gatewayId,
+                customInfo: gatewayLocation.gatewayId
+				
 			});
-		});
-		marker.addListener('click', function() {
-			var gatewayId=marker.getTitle();
-			window.open('analytics.html','_blank');  
-			map.setCenter(marker.getPosition());
-			/*return function() {
-				infowindow.setContent(gatewayLocation);
-				infowindow.open(map, marker);
-			}*/
-			window.localStorage.setItem('gateway',gatewayId);
+			marker.addListener('click', function() {
+			    alert(this.getTitle());
+				window.localStorage.removeItem('gateway');
+                var gatewayId=this.getTitle();
+            console.log("Item: "+window.localStorage.getItem('gateway'));
+            	window.localStorage.setItem('gateway',gatewayId);
+            console.log("Item1: "+window.localStorage.getItem('gateway'));
+			 	window.open('analytics.html','_blank');  
+             map.setCenter(marker.getPosition());
+          /*return function() {
+          infowindow.setContent(gatewayLocation);
+          infowindow.open(map, marker);
+        }*/
+        
+   	        
         });
+		});
+		
 	}).fail(function(data) {
 		console.log (data);
 	});
 }
+
+
 function getItem()
 {  
 	return window.localStorage.getItem('gateway');
 }
 
 var TemperatureInit = function () 
-{
+{   var gatewayId = window.localStorage.getItem('gateway');
 	var wholeTempInfo = [];
 
     $.ajax({
 		method: 'GET',
-		url: '/api/sensorData/v1.0/temperature'
+		url: '/api/sensorData/v1.0/temperature/'+gatewayId
 	}).done(function(data) {
-
+        
 		if (data.status === "OK") {
 			for(var i = 0; i < data.results.temp.length; i++){
 				
 				console.log(data.results);
-			    temperatureData = { temperature: data.results.temp[i], gatewayId: data.results.gatId[i] ,quality: data.results.airQuality[i]};
-				
+			    temperatureData = { temperature: data.results.temp[i] ,quality: data.results.airQuality[i]};
+			    var key1=gatewayId+"temperature_dashboard";
+			    window.localStorage.setItem(key1,temperatureData.temperature);
+
 			    console.log ("SecurIoT temperature data: " + JSON.stringify(temperatureData));
 			    wholeTempInfo.push (temperatureData);
+
+			    var key=gatewayId+"temperature";
+
+			    if(temperatureData.quality>=60)
+			    	{
+			    	 	window.localStorage.setItem(key,"green");
+					}	
+					else if(temperatureData.quality>=40 && temperatureData.quality<60)
+			   		{
+			   		 	window.localStorage.setItem(key,"yellow");
+					}
+					else
+					{
+			    		window.localStorage.setItem(key,"red");
+					}
+	
 				
 			}
 		} else {
 			
-			
+			console.log(data.results);
 		}
 		
 	}).fail(function(data) {
@@ -113,27 +245,43 @@ var TemperatureInit = function ()
 }
 
 var HumidityInit = function () 
-{
+{   var gatewayId = window.localStorage.getItem('gateway');
 	var wholeHumidInfo = [];
 
     $.ajax({
 		method: 'GET',
-		url: '/api/sensorData/v1.0/humidity'
+		url: '/api/sensorData/v1.0/humidity/'+gatewayId
 	}).done(function(data) {
 
 		if (data.status === "OK") {
 			for(var i = 0; i < data.results.humid.length; i++){
 				
 				console.log(data.results);
-				humidityData = { humidity: data.results.humid[i], gatewayId: data.results.gatId[i] ,quality: data.results.airQuality[i]};
+				humidityData = { humidity: data.results.humid[i],quality: data.results.airQuality[i]};
 				
 			    console.log ("SecurIoT humidity data: " + JSON.stringify(humidityData));
 			    wholeHumidInfo.push (humidityData);
+			    
+			     var key=gatewayId+"humidity";
+			    
+			    if(humidityData.quality>=60)
+			    {
+			    		window.localStorage.setItem(key,"green");
+				}		
+					else if(humidityData.quality>=40 && humidityData.quality<60)
+			    {
+			   			 window.localStorage.setItem(key,"yellow");
+				}
+					else
+				{
+			    		window.localStorage.setItem(key,"red");
+				}
+			    
 				
 			}
 		} else {
 			
-			
+			console.log(data.results);
 		}
 		
 	}).fail(function(data) {
@@ -142,26 +290,41 @@ var HumidityInit = function ()
 }
 
 var So2Init = function () 
-{
+{   var gatewayId = window.localStorage.getItem('gateway');
 	var wholeSo2Info = [];
 
     $.ajax({
 		method: 'GET',
-		url: '/api/sensorData/v1.0/so2'
+		url: '/api/sensorData/v1.0/so2/'+gatewayId
 	}).done(function(data) {
 
 		if (data.status === "OK") {
 			for(var i = 0; i < data.results.so2.length; i++){
 				
 				console.log(data.results);
-			    so2Data = { so2: data.results.so2[i], gatewayId: data.results.gatId[i] ,quality: data.results.airQuality[i]};
+			    so2Data = { so2: data.results.so2[i],quality: data.results.airQuality[i]};
 				
 			    console.log ("SecurIoT So2 data: " + JSON.stringify(so2Data));
-			    wholeTempInfo.push (so2Data);
+			    wholeSo2Info.push (so2Data);
+
+			     var key=gatewayId+"so2";
+			    if(so2Data.quality>=60)
+			    {
+			    		window.localStorage.setItem(key,"green");
+				}
+					else if(so2Data.quality>=40 && so2Data.quality<60)
+			    {
+			    		window.localStorage.setItem(key,"yellow");
+				}
+					else
+				{
+			    		window.localStorage.setItem(key,"red");
+				}
+			    
 				
 			}
 		} else {
-			
+			console.log(data.results);
 			
 		}
 		
@@ -171,27 +334,43 @@ var So2Init = function ()
 }
 
 var No2Init = function () 
-{
+{	var gatewayId = window.localStorage.getItem('gateway');
 	var wholeNo2Info = [];
 
     $.ajax({
 		method: 'GET',
-		url: '/api/sensorData/v1.0/no2'
+		url: '/api/sensorData/v1.0/no2/'+gatewayId
 	}).done(function(data) {
 
 		if (data.status === "OK") {
 			for(var i = 0; i < data.results.no2.length; i++){
 				
 				console.log(data.results);
-			    no2Data = { no2: data.results.no2[i], gatewayId: data.results.gatId[i] ,quality: data.results.airQuality[i]};
+			    no2Data = { no2: data.results.no2[i], quality: data.results.airQuality[i]};
 				
 			    console.log ("SecurIoT No2 data: " + JSON.stringify(no2Data));
-			    wholeTempInfo.push (no2Data);
+			    wholeNo2Info.push (no2Data);
+
+			     var key=gatewayId+"no2";
+			    
+			    if(no2Data.quality>=60)
+			    {
+			   		 window.localStorage.setItem(key,"green");
+				}	
+					else if(no2Data.quality>=40 && no2Data.quality<60)
+			    {
+			   		 	window.localStorage.setItem(key,"yellow");
+				}
+					else
+				{
+			    	 	window.localStorage.setItem(key,"red");
+				}
+			    
 				
 			}
 		} else {
 			
-			
+			alert(data.results);
 		}
 		
 	}).fail(function(data) {
